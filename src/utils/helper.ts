61eac8792ffd91ed1c26d72667921e29c1e7bf3d
Martin Ivanko <martin.ivanko@unitedclassifieds.sk>
2023-09-01 10:59:10 +0200

    create helper for fetching chart data

---
+++ src/utils/helper.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosResponse } from 'axios';
+
+interface Structure {
+  [key: string]: string;
+}
+
+const fetchChartData = async (
+  endpoint: string,
+  filters: string[],
+  structure: Structure
+): Promise<AxiosResponse['data'] | null> => {
+  try {
+    const apiParams = `filters=${filters.join(";")}&structure=${JSON.stringify(structure)}`;
+    const encodedParams = encodeURI(apiParams);
+    const url = `${endpoint}?${encodedParams}`;
+    
+    const response = await axios.get(url);
+    
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(`Failed to fetch data: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(`An error occurred while fetching data: ${error}`);
+    return null;
+  }
+};
+
+export default fetchChartData;
